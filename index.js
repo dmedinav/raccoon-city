80f0599beb6704f3a26d47aa6c71b58c99e6a2a3
arodriguez <arodriguez@previred.com>
2020-11-06 06:50:29 -0300

    script terminado

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("test");
+
+const raccoonCity = (input) => {
+    /**
+     *  Tu código va aquí
+     */
+    }
+    
+    
+    /**
+     * No modificar
+     */
+    const readline = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
+    
+    readline.question(`Ingrese la entrada para ver si sobrevives:  `, (input) => {
+      const inputArray = eval(input);
+      if(Array.isArray(inputArray)){
+        raccoonCity(eval(input))
+        readline.close()
+      }else{
+        console.log("Ingrese parámetros válidos")
+      }
+      readline.close()
+    })
